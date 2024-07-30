b3e8ea1b7702e75906455b27c4bca06942ccb0e8
Wizdan M. G. S. <wizdanshodri@protonmail.com>
2024-07-30 18:05:42 +0700

    Install store

--- llm_regex/apps/frontend/src/index.js
+++ llm_regex/apps/frontend/src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "../state/store";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("app")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
