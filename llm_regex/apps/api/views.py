21013b1db0f533c2571720ec9966d2a8e5237450
Wizdan M. G. S. <wizdanshodri@protonmail.com>
2024-07-30 14:05:14 +0700

    Update create and delete function for uploadedfile

--- llm_regex/apps/api/views.py
+++ llm_regex/apps/api/views.py
@@ -1,20 +1,52 @@
-import os
-
 from django.conf import settings
-from rest_framework import viewsets
+from django.core.files.storage import FileSystemStorage
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.exceptions import ValidationError
+
 from .models import UploadedFile
 from .serializers import UploadedFileSerializer
 
 
-class UploadedFileViewSet(viewsets.ModelViewSet):
+class UploadedFileViewSet(ModelViewSet):
     queryset = UploadedFile.objects.all()
     serializer_class = UploadedFileSerializer
 
     def perform_create(self, serializer):
+        """
+        Perform the creation of a new object using the given serializer.
+
+        Args:
+            serializer (Serializer): The serializer instance containing the data for the new object.
+
+        Raises:
+            ValidationError: If the content type of the file in the serializer data is not allowed.
+
+        Returns:
+            None
+        """
+        allowed_content_types = [
+            "text/csv",
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            "application/vnd.ms-excel",
+        ]
+
+        if serializer.validated_data["file"].content_type not in allowed_content_types:
+            raise ValidationError(
+                {"message": "Unsupported file type. Please upload a CSV or Excel file."}
+            )
+
         serializer.save()
 
     def perform_destroy(self, serializer):
-        file_path = os.path.join(settings.MEDIA_ROOT, serializer.file.name)
-        if os.path.exists(file_path):
-            os.remove(file_path)
+        """
+        Deletes a file associated with the given serializer and removes it from the file system.
+
+        Parameters:
+            serializer (Serializer): The serializer object containing the file to be deleted.
+
+        Returns:
+            None
+        """
+        if FileSystemStorage().exists(serializer.file.name):
+            FileSystemStorage().delete(serializer.file.name)
         serializer.delete()
