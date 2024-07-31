25949e52d6f6af7c19549608ce91bc9a4e703ed9
Wizdan M. G. S. <wizdanshodri@protonmail.com>
2024-07-31 11:12:01 +0700

    Clean db for each refresh

--- llm_regex/apps/frontend/views.py
+++ llm_regex/apps/frontend/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from apps.api.models import UploadedFile, NlpQuery
 
 
 def index(request):
+    # Clean database
+    uploaded_files = UploadedFile.objects.all()
+    for file in uploaded_files:
+        file.delete()
+    NlpQuery.objects.all().delete()
+
     return render(request, "frontend/index.html")
